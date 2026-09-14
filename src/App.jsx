e6b80067ebff03bf8afae3e4ea2552cfd3ca82c7
dev-mohammad78 <dev.mohammad78@gmail.com>
2026-09-15 02:20:00 +0330

    GraphQls added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const QUERY = gql`
+  query {
+    authors {
+      name
+    }
+  }
+`;
+
 function App() {
+  const response = useQuery(QUERY);
+  console.log(response);
+  
   return (
     <>
       <h1 className="bg-slate-500 font-light text-3xl">وبلاگ من</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
 import "./styles/fonts.css";
+
 import App from "./App.jsx";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://us-west-2.cdn.hygraph.com/content/cmtrc0cgp00hf06th2rhmwk2i/master",
+  }),
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 );
